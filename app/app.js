a14d2359a26c71ffffc238c9928bc07acbaf04d0
Samuel Bauer <bauersam@outlook.com>
2019-02-06 14:05:37 -0700

    Structure complete

---
+++ app/app.js
@@ -0,0 +1,27 @@
+//1.function that adds image when user presses a button and sets user choice
+
+
+
+
+
+
+
+
+//2.function for comp to choose, then display image
+
+
+
+
+
+
+
+
+//3.function to compare user choice to comp choice and give result
+//////////////////////////////////////////////////////////////////////////
+/////if user choice is same as comp choice
+////////result is a draw/tie
+/////else if user choice beats computer choice
+////////result is a win
+/////else
+////////result is a loss
+//////////////////////////////////////////////////////////////////////////
